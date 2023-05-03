ae8fb18c6bb736ab8d4299e3131bf31d1720b3db
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-03 21:16:02 +0200

    Added unit tests

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,17 @@
+import tempfile
 from django.test import TestCase
+from .models import Hero
 
-# Create your tests here.
+
+class HeroModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.hero = Hero.objects.create(
+            title='Lorem ipsum dolor sit amet',
+            subtitle='Sed tincidunt quis odio id molestie',
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt quis odio id molestie.',
+            image=tempfile.NamedTemporaryFile(suffix='.jpg').name
+        )
+
+    def test_hero_model(self):
+        data = self.hero
+        self.assertIsInstance(data, Hero)
