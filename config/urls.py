f3cf7122c2883448e0a29d089241bdb2a3033cfd
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-12-01 12:19:40 +0100

    Code refactoring

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
 urlpatterns = [
   path('', include('portfolio.urls')),
   path('admin/', admin.site.urls),
